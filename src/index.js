2790669c4165755d8b83581498c28eea26543c4e
ydzhony1 <ydzhony1@163.com>
2019-10-27 10:04:03 +0800

    update config

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 let hellodz = require('hellodz')
-const http = require('http')
 hellodz.sleep(5000)
 alert('睡眠5秒结束')
--- src/nodeme.js
+++ src/nodeme.js
@@ -1,6 +1,5 @@
 let hellodz = require('hellodz')
 const http = require('http')
-
 const hostname = '127.0.0.1'
 const port = 3000
 
